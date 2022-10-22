b88941ae4405b1a95f4b4a1a56b0d66b98dc4592
mmccabe05 <mmccabe@chicagobooth.edu>
2022-10-22 20:42:57 +0000

    addition and subtraction both work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
+  def addition
+    # content to go here
 
+    render({ :template => "add.html.erb" })
+  end
+  def addition_results
+    # content to go here
+    @first_number = params.fetch("first_number").to_f
+    @second_number = params.fetch("second_number").to_f
+    @result = @first_number + @second_number
+
+    render({ :template => "addition_results.html.erb" })
+  end
+  def subtract
+    # content to go here
+
+    render({ :template => "subtract.html.erb" })
+  end
+  def subtraction_results
+    # content to go here
+    @first_number = params.fetch("first_number").to_f
+    @second_number = params.fetch("second_number").to_f
+    @result = @second_number - @first_number
+
+    render({ :template => "subtraction_results.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/add", { :controller => "application", :action => "addition" })
+  get("/addition/results", { :controller => "application", :action => "addition_results" })
+  get("/subtract", { :controller => "application", :action => "subtract" })
+  get("/subtraction/results", { :controller => "application", :action => "subtraction_results" })
+  get("/multiple", { :controller => "application", :action => "multiply" })
+
 end
